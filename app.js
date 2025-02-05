790f4241e863464641404a0cb638cd5b93ed0fc9
JGRomero <jgrmmd@outlook.com>
2025-02-04 20:04:57 -0600

    Funcion para actulizar lista de amigos

--- app.js
+++ app.js
@@ -22,3 +22,12 @@ function agregarAmigo(){
     console.log("Array actulizado: ", amigo)
 }
 
+function listaAmigos(){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++){
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
